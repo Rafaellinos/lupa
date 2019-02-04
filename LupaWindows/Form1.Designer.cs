512dca777b32f82b9e6b2eacfde3a4b6e9ce1c42
Rafael Veloso <rafael.veloso.lino@hotmail.com>
2019-02-04 16:33:08 -0200

    magnifying glass for windows in C#/windows forms

---
+++ LupaWindows/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace PIM4SEMVER1._0.GUI
+{
+    partial class lupa
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 400);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(22, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(352, 346);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lupa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 424);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "lupa";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "lupa";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lupa_KeyDown);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ LupaWindows/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+
+namespace PIM4SEMVER1._0.GUI
+{
+    public partial class lupa : Form
+    {
+        Graphics GraficoCaptura;
+        Bitmap ImagemTemporal;
+        Point frmMover;
+        bool MoverMouse;
+        int Zoom = 2; // 1px, 2px, 3px ....
+
+        public lupa()
+        {
+            InitializeComponent();
+            this.TransparencyKey = Color.Turquoise;
+            this.BackColor = Color.Turquoise;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //
+            int achoImagem = pictureBox1.Width;
+            int AltoImagem = pictureBox1.Height;
+            //posição do mouse
+            int mouseX = MousePosition.X;
+            int mouseY = MousePosition.Y;
+            //captura tela
+            ImagemTemporal = new Bitmap(achoImagem / Zoom, AltoImagem / Zoom, System.Drawing.Imaging.PixelFormat.Format64bppPArgb);
+            GraficoCaptura = this.CreateGraphics();
+            GraficoCaptura = Graphics.FromImage(ImagemTemporal);
+            //copia a tela exata
+            GraficoCaptura.CopyFromScreen(mouseX - achoImagem / (Zoom * 2), mouseY - AltoImagem / (Zoom * 2), 0, 0, pictureBox1.Size);
+
+            //aumenta o tamanho
+            Bitmap NovaImagem = new Bitmap(achoImagem, AltoImagem);
+            GraficoCaptura = Graphics.FromImage(NovaImagem);
+            GraficoCaptura.SmoothingMode = SmoothingMode.HighQuality; //qualidade
+            GraficoCaptura.DrawImage(ImagemTemporal, new Rectangle(0, 0, achoImagem, AltoImagem));
+            pictureBox1.Image = NovaImagem;
+
+            //cria forma circular da lupa
+            Rectangle rect = new Rectangle(0, 0, achoImagem, AltoImagem);
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(rect);
+            pictureBox1.Region = new Region(path);
+
+            //painel circular
+            Rectangle rectp = new Rectangle(0, 0, panel1.Width, panel1.Height);
+            GraphicsPath pathp = new GraphicsPath();
+            pathp.AddEllipse(rectp);
+            panel1.Region = new Region(pathp);
+
+            // Lupa segue o mouse
+            this.Location = new Point(Cursor.Position.X, Cursor.Position.Y);
+
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (MoverMouse)
+            {
+                Location = new Point(Cursor.Position.X - frmMover.X, Cursor.Position.Y - frmMover.Y);
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            frmMover = new Point(Cursor.Position.X - Location.X, Cursor.Position.Y - Location.Y);
+            MoverMouse = true;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            MoverMouse = false;
+        }
+
+        private void lupa_KeyDown(object sender, KeyEventArgs e)
+        {
+            //quando pressiona uma tecla
+            if ((e.KeyCode & Keys.Up) == Keys.Up)
+            {
+                Zoom++; //aumenta zoom
+            }
+            if ((e.KeyCode & Keys.Down) == Keys.Down)
+            {
+                if (Zoom > 2)
+                {
+                    Zoom--; //diminui zoom
+                }
+            }
+            if ((e.KeyCode & Keys.Escape) == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        
+    }
+}
